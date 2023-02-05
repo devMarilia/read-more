1976554462bc5bfccdafa3d2a6b11d717ed53c4b
Marilia Pereira <Marilia.Pereira-ext@ab-Inbev.com>
2023-02-05 20:25:12 -0300

    🦄 refactor: ajusted logic project

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,7 +3,6 @@ import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../../context/AuthContext";
 import image001 from "../../assets/images/image01.avif";
 
-
 const Signin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -24,42 +23,10 @@ const Signin = () => {
   };
 
   return (
-    // <div className="max-w-[700px] mx-auto my-16 p-4">
-    //   <div>
-    //     <h1 className="text-2xl font-bold py-2">Faça login em sua conta</h1>
-    //   </div>
-    //   <form onSubmit={handleSubmit}>
-    //     <div className="flex flex-col py-2">
-    //       <label className="py-2 font-medium">Email</label>
-    //       <input
-    //         onChange={(e) => setEmail(e.target.value)}
-    //         className="border p-3"
-    //         type="email"
-    //       />
-    //     </div>
-    //     <div className="flex flex-col py-2">
-    //       <label className="py-2 font-medium">Senha</label>
-    //       <input
-    //         onChange={(e) => setPassword(e.target.value)}
-    //         className="border p-3"
-    //         type="password"
-    //       />
-    //     </div>
-    //     <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white">
-    //       Sign In
-    //     </button>
-    //   </form>
-    //   <p className="py-2">
-    //   Não tem uma conta ainda?{" "}
-    //     <Link to="/signup" className="underline">
-    //     Inscrever-se.
-    //     </Link>
-    //   </p>
-    // </div>
- <div className="flex h-screen bg-gray-200">
-      <div className="w-1/2 bg-white p-10">
 
-        <form>
+    <div className="flex h-screen bg-gray-200">
+      <div className="w-1/2 bg-white p-10">
+        <form onSubmit={handleSubmit}>
           <h2 className="text-lg font-medium mb-4">Login</h2>
           <div className="mb-4">
             <label
@@ -72,6 +39,8 @@ const Signin = () => {
               className="border border-gray-400 p-2 w-full"
               type="text"
               id="username"
+              name="username"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-4">
@@ -85,17 +54,23 @@ const Signin = () => {
               className="border border-gray-400 p-2 w-full"
               type="password"
               id="password"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <button className="bg-blue-500 text-white py-2 px-4 rounded-full">
-            Login
+          <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white">
+            Sign In
           </button>
+          <p className="py-2"/>
+          Não tem uma conta ainda?{" "}
+          <Link to="/signup" className="underline">
+            Inscrever-se.
+          </Link>
         </form>
       </div>
-      <div
-        className="w-1/2 bg-cover"
-
-      ><img src={image001} /></div>
+      <div className="w-1/2 bg-cover">
+        <img src={image001} />
+      </div>
     </div>
   );
 };
